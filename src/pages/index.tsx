e8dfaec64084b45bd1f948c3699070a6f96f9e41
sushidesu <hugemerge@gmail.com>
2020-11-02 03:25:13 +0000

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Home = () => (
+    <div>
+      <Head>
+        <title>catcount</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>hello</h1>
+    </div>
+  )
+
+export default Home
